e4509dbf4e31934541a3977a07a93779f84f58f6
JakubFaltyn <jakubfaltyn@gmail.com>
2023-04-05 02:01:20 +0200

    Rankeds added

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,10 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using GradeBook.Enums;
+using System;
 
 namespace GradeBook.GradeBooks
 {
-    internal class RankedGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
+        public RankedGradeBook(string name) : base(name)
+        {
+            this.Type = GradeBookType.Ranked;
+        }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5) throw new InvalidOperationException();
+
+            int howBetter = 0;
+            int percent = (100 * howBetter) / Students.Count;
+
+            foreach (var Student in Students)
+                if (averageGrade < Student.AverageGrade) howBetter++;
+
+            if (percent < 20) return 'A';
+            if (percent < 40) return 'B';
+            if (percent < 60) return 'C';
+            if (percent < 80) return 'D';
+            return 'F';
+        }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            else
+                base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            else
+                base.CalculateStudentStatistics(name);
+        }
     }
 }
